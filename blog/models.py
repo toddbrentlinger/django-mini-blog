4595277572810e2dd64dfd063b3f1326bea230f1
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-11-30 18:00:01 -0800

    Finishes models and runs database migrations.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import uuid # Used for unique blog post instances
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -8,9 +10,10 @@ class User(AbstractUser):
 class BlogPost(models.Model):
     """Model representing a blog post."""
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular blog post.')
     title = models.CharField(max_length=200, help_text='Enter title of blog post.')
-    post_date = models.DateField(verbose_name='Post Date')
-    author = models.ForeignKey('BlogAuthor')
+    post_date = models.DateField(auto_now_add=True, verbose_name='Post Date')
+    author = models.ForeignKey('BlogAuthor', on_delete=models.CASCADE)
     description = models.TextField(max_length=1000, help_text='Enter main content text of blog post.')
 
     class Meta:
@@ -23,22 +26,22 @@ class BlogPost(models.Model):
 
     def __str__(self):
         """String for representing the Model object."""
-        return self.title
+        return f'{self.id} - {self.title} - {self.author}'
 
 class BlogComment(models.Model):
     """Model representing a comment"""
 
-    post_date_time = models.DateTimeField(verbose_name='Post Date')
+    post_date_time = models.DateTimeField(auto_now_add=True, verbose_name='Post Date')
     author = models.CharField(max_length=200, help_text='Enter username of the blog comment.')
     description = models.TextField(max_length=1000, help_text='Enter content of blog comment.')
-    blog_post = models.ForeignKey(BlogPost, verbose_name='Blog Post')
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, verbose_name='Blog Post')
 
     class Meta:
         ordering = ['post_date_time']
 
     def __str__(self):
         """String for representing the Model object."""
-        pass
+        return f'{self.author} - {self.description}'
 
 class BlogAuthor(models.Model):
     """Model representing a blog post author."""
@@ -48,6 +51,11 @@ class BlogAuthor(models.Model):
 
     class Meta:
         ordering = ['name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of BlogAuthor."""
+        # return reverse('blog-author-detail-view', args=[str(self.id)])
+        pass
 
     def __str__(self):
         """String for representing the Model object."""
