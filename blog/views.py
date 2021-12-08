5ec56b08798af1e80bc8130e519b0eeec77359fe
Todd Brentlinger <toddbrentlinger@gmail.com>
2021-12-07 18:21:24 -0800

    Starts homepage index view and adds requirements.txt to list python dependencies.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import BlogPost, BlogAuthor, BlogComment
 
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_posts = BlogPost.objects.count()
+    num_authors = BlogAuthor.objects.count()
+    num_comments = BlogComment.objects.count()
+
+    context = {
+        'num_posts': num_posts,
+        'num_authors': num_authors,
+        'num_comments': num_comments,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context)
